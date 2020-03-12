7279956cc6e0fb0bba7f218eee6c0f0755b316c8
zachbrown12 <zachmb22@gmail.com>
2020-03-12 14:15:17 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,28 @@
+require 'pry'
+
 class StudentsController < ApplicationController
+  
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    session[:form_params] = params.inspect
+    binding.pry
+    redirect_to new_student_path
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
